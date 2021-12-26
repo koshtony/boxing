76af22e0d0de5a5a92851d382d6000c8412dd344
koshtony <tonykungu2>
2021-12-26 09:31:05 +0300

    created menu

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 import streamlit as st
 st.header("SALES MANAGEMENTTT")
+rad1 = st.radio("menu",["EMPLOYEE MANAGEMENT","DISTRIBUTION MANAGEMENT",
+                "SUPPLY MANAGEMENT","CUSTOMER INFO","MARKET INFO"
+                        ])
