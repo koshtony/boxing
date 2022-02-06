6888b93a06663f13bbeefda6be04b3fcf46b1e30
koshtony <tonykungu2@gmail.com>
2022-02-06 12:47:24 +0300

    --

--- supplier.py
+++ supplier.py
@@ -3,7 +3,7 @@ import sqlite3 as sq
 import pandas as pd
 # fetch orders made
 def incoming():
-    sc_data=pd.read_html("http://127.0.0.1:5000/data")
+    sc_data=pd.read_html("https://boxingsales.herokuapp.com/data")
     return sc_data
 def download(data):
     return data.to_csv().encode('utf-8')
